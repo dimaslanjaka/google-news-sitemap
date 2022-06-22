472efc1731686cd5ed21e093bc8443d9969a9e26
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-06-22 13:46:57 +0700

    update test case

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,7 @@
 const { default: GoogleNewsSitemap } = require("google-news-sitemap");
 
 const builder = new GoogleNewsSitemap();
+
 builder.add({
   location: "http://example.com/article1",
   title: "item 1",
@@ -8,6 +9,7 @@ builder.add({
   publication_language: "en",
   publication_name: "Dimas Lanjaka",
 });
+
 builder.add({
   location: "http://example.com/article2",
   title: "item 2",
@@ -15,4 +17,26 @@ builder.add({
   publication_date: "May 25, 2012",
   publication_name: "Dimas Lanjaka",
 });
+
+// reset
+builder.clear();
+
+// add other data
+
+builder.add({
+  location: "http://example.com/article1",
+  title: "item 1",
+  publication_date: "May 24, 2012",
+  publication_language: "en",
+  publication_name: "Dimas Lanjaka",
+});
+
+builder.add({
+  location: "http://example.com/article2",
+  title: "item 2",
+  publication_language: "en",
+  publication_date: "May 25, 2012",
+  publication_name: "Dimas Lanjaka",
+});
+
 console.log(builder.toString());
